0d3c757e6629a37a08c56a8502c219cb6e73eb9c
Checkmate50 <dgeisler50@gmail.com>
2023-10-31 18:25:50 -0400

    Parsing working

--- src/ast.rs
+++ src/ast.rs
@@ -1,6 +1,25 @@
+use std::collections::HashMap;
+
+pub type Type = String;
+pub type Var = String;
+
+#[derive(Debug, Clone)]
+pub enum Allocation {
+    Single, // ? -- one allocation of any type
+    Open,   // ??? -- any number of allocations
+    Type(Type), // an allocation of exactly type Type
+    Var(Var)  // explicit allocation
+}
+
+#[derive(Debug, Clone)]
+pub enum Operation {
+    Allocation(Allocation),
+    Scope(Vec<Operation>)
+}
+
 #[derive(Debug, Clone)]
 pub struct Program {
-    pub types: String,
-    pub dependencies: String,
-    pub allocations: String
+    pub types: HashMap<Var, String>,
+    pub dependencies: HashMap<Var, Vec<Var>>,
+    pub allocations: Vec<Operation>
 }
--- src/parser.rs
+++ src/parser.rs
@@ -6,6 +6,7 @@ use pest_consume::{match_nodes, Error, Parser};
 struct ColoringParser;
 
 use crate::ast;
+use std::collections::HashMap;
 
 #[derive(Clone, Debug)]
 struct UserData {}
@@ -18,22 +19,102 @@ impl ColoringParser {
     fn EOI(_input: Node) -> ParseResult<()> {
         Ok(())
     }
+    fn single(_input: Node) -> ParseResult<()> {
+        Ok(())
+    }
+    fn open(_input: Node) -> ParseResult<()> {
+        Ok(())
+    }
 
-    fn types(input: Node) -> ParseResult<String> {
+    fn var(input: Node) -> ParseResult<String> {
         input.as_str().parse::<String>().map_err(|e| input.error(e))
     }
 
-    fn dependencies(input: Node) -> ParseResult<String> {
+    fn n(input: Node) -> ParseResult<usize> {
+        input.as_str().parse::<usize>().map_err(|e| input.error(e))
+    }
+
+    fn typ(input: Node) -> ParseResult<String> {
         input.as_str().parse::<String>().map_err(|e| input.error(e))
     }
 
-    fn allocations(input: Node) -> ParseResult<String> {
-        input.as_str().parse::<String>().map_err(|e| input.error(e))
+    fn types(input: Node) -> ParseResult<HashMap<ast::Var, ast::Type>> {
+        let error = input.error("Duplicate types");
+        match_nodes!(input.into_children();
+            [type_decl(typs)..] => {
+                let mut result = HashMap::new();
+                for (var, typ) in typs {
+                    if result.insert(var.clone(), typ) != None {
+                        return Err(error);
+                    }
+                }
+                Ok(result)
+            }
+        )
     }
 
-    fn program(input : Node) -> ParseResult<ast::Program> {
+    fn type_decl(input: Node) -> ParseResult<(ast::Var, ast::Type)> {
         Ok(match_nodes!(input.into_children();
-            [types(t), dependencies(d), allocations(a), EOI(_)] => ast::Program {
+            [var(var), typ(typ)] => (var, typ)
+        ))
+    }
+
+    fn dependencies(input: Node) -> ParseResult<HashMap<ast::Var, Vec<ast::Var>>> {
+        match_nodes!(input.into_children();
+            [dependency(deps)..] => {
+                let mut result = HashMap::new();
+                for (var1, var2) in deps {
+                    let v = result.entry(var1).or_insert_with(|| Vec::new());
+                    v.push(var2);
+                }
+                Ok(result)
+            }
+        )
+    }
+
+    fn dependency(input: Node) -> ParseResult<(ast::Var, ast::Var)> {
+        Ok(match_nodes!(input.into_children();
+            [var(var1), var(var2)] => (var1, var2)
+        ))
+    }
+
+    fn operations(input: Node) -> ParseResult<Vec<ast::Operation>> {
+        Ok(match_nodes!(input.into_children();
+            [operation(ops)..] => ops.collect()
+        ))
+    }
+
+    fn operation(input: Node) -> ParseResult<ast::Operation> {
+        Ok(match_nodes!(input.into_children();
+            [allocation(allocation)] => ast::Operation::Allocation(allocation),
+            [scope(operations)] => ast::Operation::Scope(operations)
+        ))
+    }
+
+    fn allocation(input: Node) -> ParseResult<ast::Allocation> {
+        Ok(match_nodes!(input.into_children();
+            [alloc(alloc)] => alloc
+        ))
+    }
+
+    fn alloc(input: Node) -> ParseResult<ast::Allocation> {
+        Ok(match_nodes!(input.into_children();
+            [single(_)] => ast::Allocation::Single,
+            [open(_)] => ast::Allocation::Open,
+            [typ(typ)] => ast::Allocation::Type(typ),
+            [var(var)] => ast::Allocation::Var(var)
+        ))
+    }
+
+    fn scope(input: Node) -> ParseResult<Vec<ast::Operation>> {
+        Ok(match_nodes!(input.into_children();
+            [operations(operations)] => operations
+        ))
+    }
+
+    fn program(input: Node) -> ParseResult<ast::Program> {
+        Ok(match_nodes!(input.into_children();
+            [types(t), dependencies(d), operations(a), EOI(_)] => ast::Program {
                 types: t,
                 dependencies: d,
                 allocations: a
